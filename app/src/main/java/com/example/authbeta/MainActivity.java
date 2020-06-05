0d143719cbbf3ec658ac0ae0a6ccaed0c36e5263
Joaquin <sol19008@byui.edu>
2020-06-05 01:36:01 -0300

    Adding comments in the MainActivity file

--- app/src/main/java/com/example/authbeta/MainActivity.java
+++ app/src/main/java/com/example/authbeta/MainActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//This activity will have 2 buttons, one fot Doctor and for Patient
 public class MainActivity extends AppCompatActivity {
 
+    // Declare buttons
     private Button mBtnDoctor, mBtnPatient;
 
     @Override
@@ -16,16 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //connect buttons with the UI elements
         mBtnDoctor = (Button) findViewById(R.id.btnDoctor);
         mBtnPatient = (Button) findViewById(R.id.btnPatient);
 
+        //this method occur when the user clicks the Doctor Button
         mBtnDoctor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //startActivity will lead to the register page
                 startActivity(new Intent(MainActivity.this, RegisterDoctor.class));
 
             }
         });
+
+        //this method occurs when the user clicks the Patient button
         mBtnPatient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
