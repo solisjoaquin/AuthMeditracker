e58c244b082d52f1f7864967096a400e7b9eeabc
Chones17 <65252367+Chones17@users.noreply.github.com>
2020-06-10 22:49:09 -0600

    Create four validation tests for email and password.

--- app/src/main/java/com/example/authbeta/MainActivity.java
+++ app/src/main/java/com/example/authbeta/MainActivity.java
@@ -1,44 +1,42 @@
 package com.example.authbeta;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-//This activity will have 2 buttons, one fot Doctor and for Patient
+/**
+ * Creates the initial app activity and creates buttons for Doctor's and Patients.
+ */
 public class MainActivity extends AppCompatActivity {
-
-    // Declare buttons
-    private Button mBtnDoctor, mBtnPatient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //connect buttons with the UI elements
-        mBtnDoctor = (Button) findViewById(R.id.btnDoctor);
-        mBtnPatient = (Button) findViewById(R.id.btnPatient);
+        // Declare buttons.
+        Button mBtnDoctor = (Button) findViewById(R.id.btnDoctor);
+        Button mBtnPatient = (Button) findViewById(R.id.btnPatient);
 
-        //this method occur when the user clicks the Doctor Button
+        // This method occurs when the user clicks the Doctor button.
         mBtnDoctor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //startActivity will lead to the register page
+                // startActivity will lead to the Doctor registration page.
                 startActivity(new Intent(MainActivity.this, RegisterDoctor.class));
-
             }
         });
 
-        //this method occurs when the user clicks the Patient button
+        // This method occurs when the user clicks the Patient button.
         mBtnPatient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RegisterPatient.class));
 
+                // startActivity will lead to the Patient registration page.
+                startActivity(new Intent(MainActivity.this, RegisterPatient.class));
             }
         });
     }
--- app/src/main/java/com/example/authbeta/RegisterDoctor.java
+++ app/src/main/java/com/example/authbeta/RegisterDoctor.java
@@ -2,60 +2,55 @@ package com.example.authbeta;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterDoctor extends AppCompatActivity {
 
-    //These variables are 4 EditText (Name, Hospital, Email, Password) and 2 button (Register, Login)
-    private EditText mRegisterName, mRegisterEmail, mRegisterPsssword, mRegisterHospital;
-    private Button mRegisterBtn, mToLoginButton;
+    // Declare EditText variables: Name, Hospital, Email and Password.
+    private EditText mRegisterName = (EditText) findViewById(R.id.editTextRegisterName);
+    private EditText mRegisterEmail = (EditText) findViewById(R.id.editTextRegisterEmail);
+    private EditText mRegisterPassword = (EditText) findViewById(R.id.editTextRegisterPassword);
+    private EditText mRegisterHospital = (EditText) findViewById(R.id.editTextHospital);
 
-    // This is the first step for Authentication for Firebase
+    // Declare Button variables.
+    private Button mRegisterBtn = (Button) findViewById(R.id.registerBtn);
+    private Button mToLoginButton = (Button) findViewById(R.id.toLoginBtn);
+
+    // Declare Firebase authentication variable.
     private FirebaseAuth mAuth;
-    // This is the first step to work with the database
-    private DatabaseReference mDatatabe;
 
-    private String name = "";
-    private String hospital = "";
-    private String email  = "";
-    private String password = "";
+    // Declare Database variable.
+    private DatabaseReference mDatabase;
+
+    // Declare String variables.
+    private String name;
+    private String hospital;
+    private String email;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_doctor);
 
-        // connect elements (editText, button) with their variables
-
-        mRegisterName = (EditText) findViewById(R.id.editTextRegisterName);
-        mRegisterHospital = (EditText) findViewById(R.id.editTextHospital);
-        mRegisterEmail = (EditText) findViewById(R.id.editTextRegisterEmail);
-        mRegisterPsssword = (EditText) findViewById(R.id.editTextRegisterPassword);
-
-        mRegisterBtn = (Button) findViewById(R.id.registerBtn);
-        mToLoginButton = (Button) findViewById(R.id.toLoginBtn);
-
-        // Initialize Firebase Auth
+        // Initialize instance of Firebase authentication.
         mAuth = FirebaseAuth.getInstance();
 
-        //get instance of Database
-        mDatatabe = FirebaseDatabase.getInstance().getReference();
+        // Initialize instance of Database.
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,7 +59,7 @@ public class RegisterDoctor extends AppCompatActivity {
                 name = mRegisterName.getText().toString();
                 hospital = mRegisterHospital.getText().toString();
                 email = mRegisterEmail.getText().toString();
-                password = mRegisterPsssword.getText().toString();
+                password = mRegisterPassword.getText().toString();
 
                 if (!name.isEmpty() && !email.isEmpty() && !password.isEmpty() && !hospital.isEmpty()){
 
@@ -111,7 +106,7 @@ public class RegisterDoctor extends AppCompatActivity {
 
                             String id = mAuth.getCurrentUser().getUid();
 
-                            mDatatabe.child("Doctors").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            mDatabase.child("Doctors").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
                                 public void onComplete(@NonNull Task<Void> task2) {
                                     if (task2.isSuccessful()) {
