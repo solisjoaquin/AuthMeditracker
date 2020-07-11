8b3f38a766cfd7eeb2cbaa82e1c5a8c632377906
Joaquin <sol19008@byui.edu>
2020-07-11 16:22:12 -0300

    Changing the design of layouts: * Remove title bar * set a blue color

--- app/src/main/java/com/example/authbeta/LoginUser.java
+++ app/src/main/java/com/example/authbeta/LoginUser.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -30,6 +32,8 @@ public class LoginUser extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_patient);
+
+
 
         mAuth = FirebaseAuth.getInstance();
 
--- app/src/main/java/com/example/authbeta/RegisterPatient.java
+++ app/src/main/java/com/example/authbeta/RegisterPatient.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -39,6 +41,7 @@ public class RegisterPatient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_patient);
+
 
         mRegisterName = (EditText) findViewById(R.id.editTextRegisterName);
         mRegisterEmail = (EditText) findViewById(R.id.editTextRegisterEmail);
